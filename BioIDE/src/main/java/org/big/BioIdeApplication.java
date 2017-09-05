d68da09331b0e96717a0d99e4e7f422953aed70c
WangTianshan <tianshanroy@163.com>
2017-09-05 16:02:52 +0800

    创建dev分支

--- BioIDE/src/main/java/org/big/BioIdeApplication.java
+++ BioIDE/src/main/java/org/big/BioIdeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BioIdeApplication {
 
 	public static void main(String[] args) {
+		//dev
 		SpringApplication.run(BioIdeApplication.class, args);
 	}
 }

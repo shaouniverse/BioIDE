234e7c76494cb8ffce7c1396ac3a9e1497581008
WangTianshan <tianshanroy@163.com>
2018-05-25 10:42:26 +0800

    处理冲突、修改部分文字

---
+++ BioIDE/src/main/resources/static/js/taxon/distributiondataScript.js
@@ -0,0 +1,94 @@
+/**
+ * Created by Tianshan on 18-5-22.
+ */
+
+//选择关系
+function selectRelation(distributiondataNum, relationId, relationText) {
+    $("#relationId_" + distributiondataNum).val(relationId);
+    $("#relationBtn_" + distributiondataNum).text(relationText);
+}
+//删除一个新描述
+function removeDistributiondata(distributiondataNum) {
+    $("#distributiondataForm_" + distributiondataNum).remove();
+}
+//提交一个描述
+function submitDistributiondata(distributiondataNum) {
+    distributiondataFormValidator(distributiondataNum);
+    if($('#distributiondataForm_'+distributiondataNum).data('bootstrapValidator').isValid()){
+        //处理ajax提交
+        //
+        //
+        //
+        layer.msg('提交成功，请继续填写其他内容',
+            {
+                time: 1500, //1.5s后自动关闭
+            },
+            function(){
+                $('#distributiondataCollapseTitle_'+distributiondataNum).trigger("click");
+                $('#distributiondataForm_'+distributiondataNum).removeClass("panel-default");
+                $('#distributiondataForm_'+distributiondataNum).addClass("panel-success");
+                $('#distributiondataStatus_'+distributiondataNum).removeClass("hidden");
+                return true;
+            });
+        return true;
+    }
+    else{
+        $('#distributiondataForm_'+distributiondataNum).removeClass("panel-success");
+        $('#distributiondataForm_'+distributiondataNum).addClass("panel-default");
+        $('#distributiondataStatus_'+distributiondataNum).addClass("hidden");
+        layer.msg("请完成此文本描述的填写", function(){
+        });
+        return false;
+    }
+}
+
+//提交所有distributiondata
+function submitAllDistributiondata() {
+    var submitResult=true;
+    $("form[id^='distributiondataForm_']").each(function () {
+        if(submitDistributiondata($(this).attr("id").substr($(this).attr("id").lastIndexOf("_")+1))){
+        }
+        else{
+            submitResult= false;
+        }
+    });
+    return submitResult;
+}
+//添加一个新描述
+function addDistributiondata() {
+
+    var countDistributiondata = parseInt($('#countDistributiondata').val());
+
+    var thisDistributiondataNum = {
+        num: countDistributiondata + 1
+    };
+
+    $('#distributiondataForm').tmpl(thisDistributiondataNum).appendTo('#newDistributiondata');
+
+    $("#destypeid_" + (countDistributiondata + 1)).select2({
+        placeholder: "请选择描述类型"
+    });
+    $("#licenseid_" + (countDistributiondata + 1)).select2({
+        placeholder: "请选择共享协议"
+    });
+    $("#language_" + (countDistributiondata + 1)).select2({
+        placeholder: "请选择描述语言"
+    });
+    $("#distributiondatasourcesid_" + (countDistributiondata + 1)).select2({
+        placeholder: "请选择数据来源"
+    });
+    $("#relationDes_" + (countDistributiondata + 1)).select2({
+        placeholder: "请选择描述"
+    });
+
+    $('#countDistributiondata').val(countDistributiondata + 1);
+
+    addDistributiondataValidator(countDistributiondata + 1);
+
+    //title变化后处理的函数
+    $("#destitle_" + (countDistributiondata + 1)).on("change",
+        function() {
+            $("#distributiondataCollapseTitle_" + (countDistributiondata + 1)).text($("#destitle_" + (countDistributiondata + 1)).val());
+        });
+
+}

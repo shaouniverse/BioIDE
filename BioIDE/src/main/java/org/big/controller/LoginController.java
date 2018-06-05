aca7a75a942086adbe17dda60e9faf3bed667b46
WangTianshan <tianshanroy@163.com>
2018-06-05 16:19:57 +0800

    修改注册页面的有问题前端；删掉国际化配置文件

--- BioIDE/src/main/java/org/big/controller/LoginController.java
+++ BioIDE/src/main/java/org/big/controller/LoginController.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- *<p><b>注册Controller类</b></p>
- *<p> 注册相关的Controller</p>
+ * <p><b>注册Controller类</b></p>
+ * <p> 注册相关的Controller</p>
+ *
  * @author BINZI
- *<p>Created date: 2018/03/05 11：46</p>
- *<p>Copyright: The Research Group of Biodiversity Informatics (BiodInfo Group) - 中国科学院动物研究所生物多样性信息学研究组</p>
+ *         <p>Created date: 2018/03/05 11：46</p>
+ *         <p>Copyright: The Research Group of Biodiversity Informatics (BiodInfo Group) - 中国科学院动物研究所生物多样性信息学研究组</p>
  * @version: 0.1
  * @since JDK 1.80_144
  */
@@ -24,33 +25,31 @@ public class LoginController {
     private HttpServletRequest request;
 
     /**
-     *<b>登录页面</b>
-     *<p> 登录页面</p>
-     * @author WangTianshan (王天山)
+     * <b>登录页面</b>
+     * <p> 登录页面</p>
+     *
      * @param model 初始化模型
      * @return java.lang.String
+     * @author WangTianshan (王天山)
      */
-    @RequestMapping(value="", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String login(Model model) {
-    	String loginErrorMsg="";
-        try{
-            if(request.getSession().getAttribute("loginError").equals("name")){
+        String loginErrorMsg = "";
+        try {
+            if (request.getSession().getAttribute("loginError").equals("name")) {
                 model.addAttribute("loginError", 1);
-                loginErrorMsg="无此用户名";
-            }
-            else if(request.getSession().getAttribute("loginError").equals("password")){
+                loginErrorMsg = "无此用户名";
+            } else if (request.getSession().getAttribute("loginError").equals("password")) {
                 model.addAttribute("loginError", 1);
-                loginErrorMsg="密码错误";
-            }
-            else if(request.getSession().getAttribute("loginError").equals("token")){
+                loginErrorMsg = "密码错误";
+            } else if (request.getSession().getAttribute("loginError").equals("token")) {
                 model.addAttribute("loginError", 1);
-                loginErrorMsg="验证码错误";
-            }
-            else{
+                loginErrorMsg = "验证码错误";
+            } else {
                 model.addAttribute("loginError", 0);
             }
             model.addAttribute("loginErrorMsg", loginErrorMsg);
-        }catch(Exception e){
+        } catch (Exception e) {
         }
         return "login";
     }
--- BioIDE/src/main/java/org/big/controller/RegisterController.java
+++ BioIDE/src/main/java/org/big/controller/RegisterController.java
@@ -17,72 +17,75 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
-    
-	@Autowired
+
+    @Autowired
     private UserService userService;
-    @Value("${spring.mail.username}") 	// BINZIDYB01@yeah.net
+    @Value("${spring.mail.username}")    // BINZIDYB01@yeah.net
     private String fromEmail;
-    
+
     /**
-     *<b>注册页面</b>
-     *<p> 注册页面</p>
-     * @author WangTianshan (王天山)
+     * <b>注册页面</b>
+     * <p> 注册页面</p>
+     *
      * @param model 空的model
      * @return java.lang.String
-     */
-	@RequestMapping(value = "", method = { RequestMethod.GET })
-	public String register(Model model) {
-		User newUser = new User();
-		newUser.setRole("ROLE_USER");
-		model.addAttribute("newUser", newUser); // model到浏览器 -- 用于权限
-		return "user/register";
-	}
-
-	@RequestMapping(value = "/new", method = { RequestMethod.POST })
-	public String newUser(HttpServletRequest request, HttpServletResponse response,
-			@ModelAttribute("newUser") User newUser, Model model) {
-		String registerMsg = this.userService.registerNewOne(request, response, newUser);
-		if (registerMsg.equals("success")) {
-			request.getSession().setAttribute("registerEmail", newUser.getEmail());
-			request.getSession().setAttribute("adminEmail", fromEmail);
-			return "redirect:/register/success";
-		} else {
-			model.addAttribute("newUser", newUser);
-			model.addAttribute("errorMsg", registerMsg);
-			return "user/register";
-		}
-	}
-	
-	/**
-     *<b>注册成功页面</b>
-     *<p> 注册成功页面</p>
      * @author WangTianshan (王天山)
-     * @return java.lang.String
      */
-    @RequestMapping(value="/success")
-    public String success(Model model,HttpServletRequest request) {
+    @RequestMapping(value = "", method = {RequestMethod.GET})
+    public String register(Model model) {
+        User newUser = new User();
+        newUser.setRole("ROLE_USER");
+        model.addAttribute("newUser", newUser); // model到浏览器 -- 用于权限
+        return "user/register";
+    }
+
+    @RequestMapping(value = "/new", method = {RequestMethod.POST})
+    public String newUser(HttpServletRequest request, HttpServletResponse response,
+                          @ModelAttribute("newUser") User newUser, Model model) {
+        String registerMsg = this.userService.registerNewOne(request, response, newUser);
+        if (registerMsg.equals("success")) {
+            request.getSession().setAttribute("registerEmail", newUser.getEmail());
+            request.getSession().setAttribute("adminEmail", fromEmail);
+            return "redirect:/register/success";
+        } else {
+            model.addAttribute("newUser", newUser);
+            model.addAttribute("errorMsg", registerMsg);
+            return "user/register";
+        }
+    }
+
+    /**
+     * <b>注册成功页面</b>
+     * <p> 注册成功页面</p>
+     *
+     * @return java.lang.String
+     * @author WangTianshan (王天山)
+     */
+    @RequestMapping(value = "/success")
+    public String success(Model model, HttpServletRequest request) {
         return "user/registerSuccess";
     }
-    
+
     /**
-     *<b>激活结果页面</b>
-     *<p> 激活结果页面</p>
-     * @author WangTianshan (王天山)
+     * <b>激活结果页面</b>
+     * <p> 激活结果页面</p>
+     *
      * @return java.lang.String
+     * @author WangTianshan (王天山)
      */
     // http://localhost:8081/register/active/BINZI/437b1ebe-cd79-4979-a5ca-73071f6368a3/
-	@RequestMapping(value = "/active/{userName}/{mark}")
-	public String active(Model model, HttpServletRequest request, HttpServletResponse response,
-			@PathVariable("userName") String userName, @PathVariable("mark") String mark) {
-		String activeMsg = userService.activeUser(userName, mark, request, response); // 返回用户状态
-		model.addAttribute("activeMsg", activeMsg);
-		request.getSession().setAttribute("adminEmail", fromEmail);
-		if (activeMsg.equals("账户已激活")) {
-			model.addAttribute("activeStatus", true);
-			model.addAttribute("loginUsername", userName);
-		} else {
-			model.addAttribute("activeStatus", false);
-		}
-		return "/login"; // 激活成功跳到登录页面 -- 该页面提示注册用户 -- "用户已激活"
-	}
+    @RequestMapping(value = "/active/{userName}/{mark}")
+    public String active(Model model, HttpServletRequest request, HttpServletResponse response,
+                         @PathVariable("userName") String userName, @PathVariable("mark") String mark) {
+        String activeMsg = userService.activeUser(userName, mark, request, response); // 返回用户状态
+        model.addAttribute("activeMsg", activeMsg);
+        request.getSession().setAttribute("adminEmail", fromEmail);
+        if (activeMsg.equals("账户已激活")) {
+            model.addAttribute("activeStatus", true);
+            model.addAttribute("loginUsername", userName);
+        } else {
+            model.addAttribute("activeStatus", false);
+        }
+        return "/login"; // 激活成功跳到登录页面 -- 该页面提示注册用户 -- "用户已激活"
+    }
 }
